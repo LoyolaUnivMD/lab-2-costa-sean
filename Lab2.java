e0c6b4d975c70d4d8cb5c48b9968825d11eff029
seantriplett <srtriplett@loyola.edu>
2024-01-31 13:53:45 -0500

    Added Comments

--- Lab2.java
+++ Lab2.java
@@ -13,29 +13,44 @@ import java.text.DecimalFormat;
 
 class Lab2 {
     public static void main(String[] args) {
+
+        //Initialize prize and create objects from imports
         long prize = 225938745L;
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
         DecimalFormat twoDigits = new DecimalFormat("00");
 
+        //Output purpose of code
         System.out.println("CS 212 - Lab 2");
         System.out.println("This program generates 10 lottery tickets.");
-        
+
+        //Prompt user to enter name
         System.out.print("What's your name? ");
 
+        //Intake name from user and trim
         String customerName = input.nextLine().trim();
+
+        //Check if name contains a break
         if(customerName.indexOf(' ') > 0) {
+            //Take index of break and set customer name to first name
             int index = customerName.indexOf(' ');
             customerName = customerName.substring(0, index);
         }
 
+        //Loop 10 times
         for (int i = 0; i < 10; i++) {
+            //Loop 6 times
             for (int j = 0; j < 6; j++) {
+                //Generate random number between 0 and 99
                 int lotteryNumber = rand.nextInt(99);
+                //Print random number using decimalformat
                 System.out.print(twoDigits.format(lotteryNumber) + " ");
             }
+            //Print new line
             System.out.println();
         }
+
+        //Output final statement with estimated jackpot
         System.out.println("-----------------");
         System.out.println("Good luck " + customerName +"!");
         System.out.println("Estimated Jackpot: ");
