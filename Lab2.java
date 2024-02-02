b7c3817a0266627632336850ab26346bf664560a
Costamarmar <Cmarmaras1@gmail.com>
2024-02-01 21:34:12 -0500

    Added the winnings section of the code and also comments since it wasn't showing up on mine

--- Lab2.java
+++ Lab2.java
@@ -5,7 +5,8 @@
 // Problem Statement: Create a program that creates 10 different lottery tickets and the prize pool
 // Data In: users name
 // Data Out: 10 lottery tickets, a good luck message, the prize pool
-// Credits: Notes #3, page 4
+// Credits: [Is your code based on an example in the book, in class, or something else?
+//            Reminder: you should never take code from the Internet or another person
 
 import java.util.Scanner;
 import java.util.Random;
@@ -13,50 +14,57 @@ import java.text.DecimalFormat;
 
 class Lab2 {
     public static void main(String[] args) {
-
-        //Initialize prize and create objects from imports
-        long prize = 225938745L;
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
+        //The different number formats needed for this
         DecimalFormat twoDigits = new DecimalFormat("00");
+        DecimalFormat money = new DecimalFormat("$.00");
+        DecimalFormat winnings = new DecimalFormat("$#,###.##");
 
-        //Output purpose of code
         System.out.println("CS 212 - Lab 2");
         System.out.println("This program generates 10 lottery tickets.");
 
-        //Prompt user to enter name
+        //asks for the name of the user and the money that they would like to play
         System.out.print("What's your name? ");
-
-        //Intake name from user and trim
         String customerName = input.nextLine().trim();
 
-        //Check if name contains a break
+        System.out.println("How much money would you like to play?");
+        double bet = input.nextDouble();
+
+        //seperates the last name from the middle name
         if(customerName.indexOf(' ') > 0) {
-            //Take index of break and set customer name to first name
             int index = customerName.indexOf(' ');
             customerName = customerName.substring(0, index);
         }
 
-        //Loop 10 times
+        System.out.println("Great! " + customerName + " wants to play " + money.format(bet) + "!");
+        System.out.println("\nHere are the tickets\n-----------------");
+
+        //loops 10 times for the 10 tickets, loops 6 times inside for the 6 numbers for each tickets numbers
         for (int i = 0; i < 10; i++) {
-            //Loop 6 times
             for (int j = 0; j < 6; j++) {
-                //Generate random number between 0 and 99
                 int lotteryNumber = rand.nextInt(99);
-                //Print random number using decimalformat
+
+                /*
+                switch statement which will put the bet to the power of 1.75 if it is one of the winning numbers
+                */
+                switch(lotteryNumber)
+                {
+                    case 3, 5, 11, 16, 58, 59:
+                        bet = Math.pow(bet, 1.75);
+                        break;
+                }
                 System.out.print(twoDigits.format(lotteryNumber) + " ");
             }
-            //Print new line
             System.out.println();
         }
 
-        //Output final statement with estimated jackpot
+        //final goodbye and goodluck message, displays the users winnings
         System.out.println("-----------------");
         System.out.println("Good luck " + customerName +"!");
-        System.out.println("Estimated Jackpot: ");
-        System.out.println("$" + prize);
         System.out.println("-----------------");
-
+        System.out.println("Your winnings are " + winnings.format(bet));
+        System.out.println("-----------------");
 
     }
 }
